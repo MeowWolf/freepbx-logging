202b9e953d4cdc24d3d7049b57472cb6f86679c0
mstark127 <97064452+mstark127@users.noreply.github.com>
2023-08-10 16:08:04 -0400

    removed eroneous lines for simplification

--- asterisk_logging.py
+++ asterisk_logging.py
@@ -9,10 +9,7 @@ def full_log_copy():
     src_path = r'/var/log/asterisk/full'
     dest_path = r'/home/mwdevops/json/full'
     shutil.copy(src_path, dest_path)
-
-
-
-'''
+    
 #Parses log entries into a list item called log_entries
 def parse_log_file(log_file_path):
     log_entries = []
@@ -62,28 +59,6 @@ def convert_freepbx_log():
     with open("freepbx_output.json", "w") as json_file:
         json_file.write(log_json)
 
-#converts system log file from a bove into JSON format (saves file in .json)
-def convert_system_log():
-    parse_log_file("/mwdevops/home/JSON/system/system.log")
-    log_file_path = "/mwdevops/home/JSON/system/system.log"
-    log_entries = parse_log_file(log_file_path)
-
-    # Convert the list of log entries to JSON
-    log_json = json.dumps(log_entries, indent=4)
-
-    # Save the JSON to a file or print it
-    with open("system_output.json", "w") as json_file:
-        json_file.write(log_json)
-
-#copy the stuff from the next file over here
-'''
-'''def send_asterisk_json():
-    os.chdir('/mwdevops/home/json/asterisk/')
-    headers = {
-        'Content-type': 'application/json'
-               }
-    r = requests.post('https://302d63369ddc4072852f17a5a7b7771e.us-central1.gcp.cloud.es.io:443/search-twilio-english/_doc?pipeline=ent-search-generic-ingestion')'''
-'''
 #convert each line of JSON to its own file
 # List of items you want to make POST requests for
 def send_freepbx_logs():
@@ -117,4 +92,4 @@ def send_freepbx_logs():
             print(f"Successfully made POST request for {item}")
         else:
             print(f"Failed to make POST request for {item}. Status code: {response.status_code}")
-'''
+
