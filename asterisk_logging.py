0363bdeff45ec6d0e4f08c429273d00a84cd1c92
mstark127 <97064452+mstark127@users.noreply.github.com>
2023-08-11 08:50:31 -0400

    fixed tabliture in def run_full_log_copy_as_sudo fxn

--- asterisk_logging.py
+++ asterisk_logging.py
@@ -8,12 +8,13 @@ import subprocess
 #Function calls the copy_logs.py script to run the script as sudo
 def run_full_log_copy_as_sudo():
     sudo_command = "sudo python3 copy_logs.py"
+
     try:
-    subprocess.run(sudo_command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    print("Sudo command executed successfully.")
-except subprocess.CalledProcessError as e:
-    print("Error executing sudo command:", e)
-    
+        subprocess.run(sudo_command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print("Sudo command executed successfully.")
+    except subprocess.CalledProcessError as e:
+        print("Error executing sudo command:", e)
+
 #Parses log entries into a list item called log_entries
 def parse_log_file(log_file_path):
     log_entries = []
