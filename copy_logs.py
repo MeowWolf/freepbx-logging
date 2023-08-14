720cc6de8c1d7e87ab43ebecf676c12db63711a7
mstark127 <97064452+mstark127@users.noreply.github.com>
2023-08-14 10:44:11 -0400

    fixed an error in src_path and dest_path variables

--- copy_logs.py
+++ copy_logs.py
@@ -1,8 +1,8 @@
 import shutil
 
 def full_log_copy():
-    src_path = r'/var/log/asterisk/full'
-    dest_path = r'/home/mwdevops/json/full-20230727'
+    src_path = r'/var/log/asterisk/full-20230727'
+    dest_path = r'/home/mwdevops/json/full/'
     shutil.copy(src_path, dest_path)
 
 full_log_copy()
