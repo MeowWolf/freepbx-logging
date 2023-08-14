9f6ff477ad294f9e8c523e9fd46c08fb3b4d3745
mstark127 <97064452+mstark127@users.noreply.github.com>
2023-08-14 14:48:31 -0400

    added api_key and headers to code

--- log_send.py
+++ log_send.py
@@ -9,9 +9,12 @@ def send_full_logs():
 # URL where you want to make the POST requests
     url = 'https://302d63369ddc4072852f17a5a7b7771e.us-central1.gcp.cloud.es.io:443/search-vocs/_doc?pipeline=ent-search-generic-ingestion'  #depending on how much noise we can filter out it may be easier / make more sense to send data via ingest pipeline over LS
 
+    api_key = 'aVh0Z3Q0a0JiYzE3Vzl1TW9fN006MEpWc0VDUFdSaVNiM1RGOHdEWFVKQQ=='
 # Iterate through the items and make POST requests
     for item in items:
         data = {'item': item}  # You can adjust the payload data as needed
+
+        headers = {'Authorization': f'Bearer {api_key}'}
         response = requests.post(url, data=data)
 
         if response.status_code == 200:
