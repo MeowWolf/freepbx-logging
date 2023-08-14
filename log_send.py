eba86ded7daf78bdcd2a589256e5d2ca601e2b30
mstark127 <97064452+mstark127@users.noreply.github.com>
2023-08-14 12:58:02 -0400

    removed the timestamp steps to test just sending files to logstash

--- log_send.py
+++ log_send.py
@@ -4,11 +4,11 @@ def send_full_logs():
         for line in file:
             items.append(line.strip())
 
-        timestamp_keys = [key for key in items.keys() if re.match(r'timestamp\d+', key)]
+       ''' timestamp_keys = [key for key in items.keys() if re.match(r'timestamp\d+', key)]
         timestamp_values = [items[key] for key in timestamp_keys]
         sorted_timestamps = sorted(timestamp_values)
         global latest_timestamp = sorted_timestamps[-1]
-        print("Latest Timestamp:", latest_timestamp)
+        print("Latest Timestamp:", latest_timestamp)'''
 # URL where you want to make the POST requests
     url = 'http://34.174.243.169:8080'  #depending on how much noise we can filter out it may be easier / make more sense to send data via ingest pipeline over LS
 
