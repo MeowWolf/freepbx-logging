5f350f466a9f38b722fbdc193cabcb25d09d1fbb
mstark127 <97064452+mstark127@users.noreply.github.com>
2023-08-14 14:34:46 -0400

    changed from sending to logstash to sending to ingest pipeline in elastic cloud

--- log_send.py
+++ log_send.py
@@ -7,7 +7,7 @@ def send_full_logs():
             items.append(line.strip())
 
 # URL where you want to make the POST requests
-    url = 'http://34.174.243.169:8080'  #depending on how much noise we can filter out it may be easier / make more sense to send data via ingest pipeline over LS
+    url = 'https://302d63369ddc4072852f17a5a7b7771e.us-central1.gcp.cloud.es.io:443/search-vocs/_doc?pipeline=ent-search-generic-ingestion'  #depending on how much noise we can filter out it may be easier / make more sense to send data via ingest pipeline over LS
 
 # Iterate through the items and make POST requests
     for item in items:
