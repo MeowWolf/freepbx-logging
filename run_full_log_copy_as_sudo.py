96900cbb118588969ec01d57b5742c587cf1041f
mstark127 <97064452+mstark127@users.noreply.github.com>
2023-08-14 08:53:38 -0400

    Created a new python file of the first function from asterisk_logging.py for testing

---
+++ run_full_log_copy_as_sudo.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+
+def run_full_log_copy_as_sudo():
+    #needs a command to cd to whatever directory we save copy_logs.py to
+    #something like: 
+    change_directory = "/path/to/directory/file.py"
+    os.chdir(change_directory)
+    #add a print statmement for testing to confirm the cd worked
+    print("New directory is:", os.getcwd())
+    
+    sudo_command = "sudo python3 copy_logs.py"
+    
+    try:
+        subprocess.run(sudo_command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print("Sudo command executed successfully.")
+    except subprocess.CalledProcessError as e:
+        print("Error executing sudo command:", e)
