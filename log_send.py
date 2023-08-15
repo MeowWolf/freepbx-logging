35ce4347f64c296973347ac738eadbd4e57c8726
mstark127 <97064452+mstark127@users.noreply.github.com>
2023-08-15 08:54:59 -0400

    Testing to see what data is actually trying to be sent

--- log_send.py
+++ log_send.py
@@ -17,9 +17,10 @@ def send_full_logs():
         headers = {'Authorization': f'Bearer {api_key}'}
         response = requests.post(url, data=data)
 
-        if response.status_code == 200:
+       ''' if response.status_code == 200:
             print(f"Successfully made POST request for {item}")
         else:
-            print(f"Failed to make POST request for {item}. Status code: {response.status_code}")
+            print(f"Failed to make POST request for {item}. Status code: {response.status_code}")'''
+        print(response)
 
 send_full_logs()
