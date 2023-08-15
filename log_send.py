8ed87361f3e73fbdb8ec661009d66f2e895539c8
mstark127 <97064452+mstark127@users.noreply.github.com>
2023-08-15 11:52:15 -0400

    fixes for posting list as objects

--- log_send.py
+++ log_send.py
@@ -2,29 +2,30 @@ import requests
 import json
 
 def send_full_logs():
+    url = 'https://302d63369ddc4072852f17a5a7b7771e.us-central1.gcp.cloud.es.io:443/search-vocs/_doc?pipeline=ent-search-generic-ingestion'  #depending on how much noise we can filter out it may be easier / make more sense to send data via ingest pipeline over LS
+    api_key = 'aVh0Z3Q0a0JiYzE3Vzl1TW9fN006MEpWc0VDUFdSaVNiM1RGOHdEWFVKQQ=='
+    headers = {'Authorization': f'Bearer {api_key}'}
+
     items = []
     with open('log_output.json', 'r') as file:
         # Open the JSON file and read its contents
         json_data_list = json.load(file)
 
         for item in json_data_list:
-            print(item)
-#make the line.strip an object.strip(not called that) to make sure we 
-# URL where you want to make the POST requests
-    url = 'https://302d63369ddc4072852f17a5a7b7771e.us-central1.gcp.cloud.es.io:443/search-vocs/_doc?pipeline=ent-search-generic-ingestion'  #depending on how much noise we can filter out it may be easier / make more sense to send data via ingest pipeline over LS
-
-    api_key = 'aVh0Z3Q0a0JiYzE3Vzl1TW9fN006MEpWc0VDUFdSaVNiM1RGOHdEWFVKQQ=='
+            response = requests.post(url, data=item)
+            
+            if response.status_code == 200:
+                print(f"Successfully made POST request for {item}")
+            else:
+                print(f"Failed to make POST request for {item}. Status code: {response.status_code}")
 # Iterate through the items and make POST requests
-    for item in items:
-        data = {'item': item}  # You can adjust the payload data as needed
+    #for item in items:
+        #data = {'item': item}  # You can adjust the payload data as needed
 
-        headers = {'Authorization': f'Bearer {api_key}'}
-        #response = requests.post(url, data=data)
+        
+        #
 
-       #if response.status_code == 200:
-            #print(f"Successfully made POST request for {item}")
-        #else:
-            #print(f"Failed to make POST request for {item}. Status code: {response.status_code}")
+       
         #print(data)
 
 send_full_logs()
