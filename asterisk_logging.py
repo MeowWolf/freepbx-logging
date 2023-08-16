a1c09bd7531a692528b1843e4fa3acbc1dde0f91
mstark127 <97064452+mstark127@users.noreply.github.com>
2023-08-16 12:00:17 -0400

    commited edits made to json_conversion.py, log_send.py to asterisk_logging.py

--- asterisk_logging.py
+++ asterisk_logging.py
@@ -1,6 +1,5 @@
 import json
 import re
-import shutil
 import requests
 import os
 import subprocess
@@ -23,18 +22,45 @@ def run_full_log_copy_as_sudo():
         print("Error executing sudo command:", e)
 
 #Parses log entries into a list item called log_entries
-def parse_log_file(log_file_path):
+def parse_log_line(log_line):
+    # Define a regular expression pattern to match the log entry format
+    log_pattern = r'\[(.*?)\] (\w+)\[(\d+)\] (.+?): (.*)'
+    match = re.match(log_pattern, log_line)
+
+    if match:
+        timestamp = match.group(1)
+        level = match.group(2)
+        process_id = match.group(3)
+        source = match.group(4)
+        message = match.group(5)
+        return timestamp, level, process_id, source, message
+    else:
+        # Return default values or handle the case where the line doesn't match the format
+        return None, None, None, None, None
+
+
+
+def parse_log_file(log_folder_path):
     log_entries = []
 
-    with open(log_file_path, 'r') as log_file:
+    # List files in the log folder
+    log_files = [file for file in os.listdir(log_folder_path) if file.startswith('full-')]
+    sorted_log_files = sorted(log_files, key=lambda x: x.split('-')[-1])
+
+    # Use the latest log file
+    latest_log_file = sorted_log_files[-1]
+
+    with open(os.path.join(log_folder_path, latest_log_file), 'r') as log_file:
         for line in log_file:
             # Parse the log line and extract relevant information
-            timestamp, level, message = parse_log_line(line)
+            timestamp, level, process_id, source, message = parse_log_line(line)
 
             # Create a dictionary for the log entry
             log_entry = {
                 'timestamp': timestamp,
                 'level': level,
+                'process_id': process_id,
+                'source': source,
                 'message': message
             }
 
@@ -42,60 +68,37 @@ def parse_log_file(log_file_path):
 
     return log_entries
 
-#parses logs into the needed format (JSON)
-def parse_log_line(log_line):
-    # Define a regular expression pattern to match the log entry format
-    log_pattern = r'\[(.*?)\] \[(.*?)\]: (.*)'
-    match = re.match(log_pattern, log_line)
-
-    if match:
-        timestamp = match.group(1)
-        level = match.group(2)
-        message = match.group(3)
-        return timestamp, level, message
-    else:
-        # Return default values or handle the case where the line doesn't match the format
-        return None, None, None
-
-#converts freepbx log file from above into JSON format (saves file in .json)
-def convert_freepbx_log():
-    parse_log_file("/home/mwdevops/json/full/full")
-    parse_log_line()
-    log_file_path = "/home/mwdevops/json/full/full"
-    log_entries = parse_log_file(log_file_path)
+# Main function
+log_folder_path = "/home/mwdevops/json/full"
+log_entries = parse_log_file(log_folder_path)
 
     # Convert the list of log entries to JSON
-    log_json = json.dumps(log_entries, indent=4)
+log_json = json.dumps(log_entries, indent=4)
 
     # Save the JSON to a file or print it
-    with open("full_output.json", "w") as json_file:
-        json_file.write(full_json)
+with open("log_output.json", "w") as json_file:
+        json_file.write(log_json)
+
+print(log_entries[0])
 
 #convert each line of JSON to its own file
 # List of items you want to make POST requests for
 def send_full_logs():
+    url = 'https://302d63369ddc4072852f17a5a7b7771e.us-central1.gcp.cloud.es.io:443/search-vocs/_doc?pipeline=ent-search-generic-ingestion'  #depending on how much noise we can filter out it may be easier / make more sense to send data via ingest pipeline over LS
+    api_key = 'ZVhUdC1Za0Jack1ZRDc3Q2QwS1E6c0VXZEZ5cEhTNEd1U29YRE11aU53dw=='
+    headers = {'Authorization': f'ApiKey {api_key}'}
+
     items = []
-    with open('full_output.json', 'r') as file:
-        for line in file:
-            items.append(line.strip())
+    with open('log_output.json', 'r') as file:
+        # Open the JSON file and read its contents
+        json_data_list = json.load(file)
 
-        timestamp_keys = [key for key in items.keys() if re.match(r'timestamp\d+', key)]
-        timestamp_values = [items[key] for key in timestamp_keys]
-        sorted_timestamps = sorted(timestamp_values)
-        global latest_timestamp = sorted_timestamps[-1]
-        print("Latest Timestamp:", latest_timestamp)
-# URL where you want to make the POST requests
-    url = 'http://34.174.243.169:8080'  #depending on how much noise we can filter out it may be easier / make more sense to send data via ingest pipeline over LS
-
-# Iterate through the items and make POST requests
-    for item in items:
-        data = {'item': item}  # You can adjust the payload data as needed
-        response = requests.post(url, data=data)
-
-        if response.status_code == 200:
-            print(f"Successfully made POST request for {item}")
-        else:
-            print(f"Failed to make POST request for {item}. Status code: {response.status_code}")
+        for item in json_data_list:
+            response = requests.post(url, headers=headers, json=item)
+            if response.status_code == 200:
+                print(f"Successfully made POST request for {item}")
+            else:
+                print(f"Failed to make POST request for {item}. Status code: {response.status_code}")
 
 def log_send_loop():
     global latest_timestamp
