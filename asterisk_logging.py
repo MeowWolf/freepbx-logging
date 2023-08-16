662d1864680c9c034bdd05c882fabfacc9436f2a
mstark127 <97064452+mstark127@users.noreply.github.com>
2023-08-16 14:57:35 -0400

    test

--- asterisk_logging.py
+++ asterisk_logging.py
@@ -8,10 +8,10 @@ import subprocess
 def run_full_log_copy_as_sudo():
     #needs a command to cd to whatever directory we save copy_logs.py to
     #something like: 
-    change_directory = "/path/to/directory/file.py"
-    os.chdir(change_directory)
+    #change_directory = "/var//directory/file.py"
+    #os.chdir(change_directory)
     #add a print statmement for testing to confirm the cd worked
-    print("New directory is:", os.getcwd())
+    #print("New directory is:", os.getcwd())
     
     sudo_command = "sudo python3 copy_logs.py"
     
