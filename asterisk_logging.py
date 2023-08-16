f016cfaaa6960a2cae17f7abfa44f7c051e763eb
mstark127 <97064452+mstark127@users.noreply.github.com>
2023-08-16 12:19:26 -0400

    indent error fix?

--- asterisk_logging.py
+++ asterisk_logging.py
@@ -102,7 +102,6 @@ def send_full_logs():
 
 def log_send_loop():
     global latest_timestamp
-
     while True:
         run_full_log_copy_as_sudo()
         log_entries = parse_log_file("/home/mwdevops/json/full/full")
