fc4c27dfdad992905d38f7c663de49b0665c7a77
mstark127 <97064452+mstark127@users.noreply.github.com>
2023-08-16 12:11:43 -0400

    fixed an indent error

--- asterisk_logging.py
+++ asterisk_logging.py
@@ -103,7 +103,7 @@ def send_full_logs():
 def log_send_loop():
     global latest_timestamp
 
-     while True:
+    while True:
         run_full_log_copy_as_sudo()
         log_entries = parse_log_file("/home/mwdevops/json/full/full")
         convert_freepbx_log()
