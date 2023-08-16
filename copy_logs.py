3e4ecfb7ec5b1766aaee66a188d3d56835f5afd8
mstark127 <97064452+mstark127@users.noreply.github.com>
2023-08-16 09:41:56 -0400

    made edits to copy_logs to dynamically find the origin log file

--- copy_logs.py
+++ copy_logs.py
@@ -1,8 +1,25 @@
 import shutil
+import os
+from datetime import datetime
+
+def find_latest_full_log():
+    log_folder = '/var/log/asterisk'
+    full_logs = [file for file in os.listdir(log_folder) if file.startswith('full-') and re.match(r'full-\d{4}/\d{2}/\d{2}$', file)]
+    sorted_logs = sorted(full_logs, key=lambda x: datetime.strptime(x.split('-')[-1], '%Y/%m/%d'), reverse=True)
+    
+    if sorted_logs:
+        return os.path.join(log_folder, sorted_logs[0])
+    else:
+        return None
 
 def full_log_copy():
-    src_path = r'/var/log/asterisk/full-20230727'
-    dest_path = r'/home/mwdevops/json/full/'
-    shutil.copy(src_path, dest_path)
+    src_path = find_latest_full_log()
+    dest_path = '/home/mwdevops/json/full/'
+    
+    if src_path:
+        shutil.copy(src_path, dest_path)
+        print(f"Successfully copied {src_path} to {dest_path}")
+    else:
+        print("No suitable log file found.")
 
 full_log_copy()
