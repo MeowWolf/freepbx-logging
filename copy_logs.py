7f0ff701d042d492a212ab7da2108f8432be03c1
mstark127 <97064452+mstark127@users.noreply.github.com>
2023-08-16 09:47:12 -0400

    took out a date modifier in sored_logs line

--- copy_logs.py
+++ copy_logs.py
@@ -6,7 +6,7 @@ import re
 def find_latest_full_log():
     log_folder = '/var/log/asterisk'
     full_logs = [file for file in os.listdir(log_folder) if file.startswith('full-') and re.match(r'full-\d{4}/\d{2}/\d{2}$', file)]
-    sorted_logs = sorted(full_logs, key=lambda x: datetime.strptime(x.split('-')[-1], '%Y/%m/%d'), reverse=True)
+    sorted_logs = sorted(full_logs, key=lambda x: datetime.strptime(x.split('-')[-1])
     
     if sorted_logs:
         return os.path.join(log_folder, sorted_logs[0])
