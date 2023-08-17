98bc110835e56f318474fa8fee623ea9a5630e14
mstark127 <97064452+mstark127@users.noreply.github.com>
2023-08-17 10:09:41 -0400

    fixed an issue and the .sh file should now detect copies and moves to a folder as well as creates

--- change_file_perms.sh
+++ change_file_perms.sh
@@ -2,6 +2,8 @@ def change_new_file_perms():
     while True; do
         inotifywait -e create -e moved_to -e moved_from -m /home/mwdevops/json/full |
         while read path action file; do
+            if [ "$action" == "CREATE" ] || [ "$action" == "MOVED_TO" ]; then
             chmod +w "$path/$file"
+            fi
         done 
     done
