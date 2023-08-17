ed334a2ef0c38b5d2aa4878eb040968e0fcbd8a0
mstark127 <97064452+mstark127@users.noreply.github.com>
2023-08-17 10:00:59 -0400

    added changes to the inotifywait statement so that it works when a file is moved or copied to the directory as well as created

--- change_file_perms.sh
+++ change_file_perms.sh
@@ -1,6 +1,6 @@
 def change_new_file_perms():
     while True; do
-        inotifywait -e create -m /home/mwdevops/json/full |
+        inotifywait -e create -e moved_to -e moved_from -m /home/mwdevops/json/full |
         while read path action file; do
             chmod +w "$path/$file"
         done 
