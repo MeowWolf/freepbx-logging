de806e96e469283312a158cb207cd95d25cf8aee
mstark127 <97064452+mstark127@users.noreply.github.com>
2023-08-17 15:14:31 -0400

    test

--- asterisk_logging.py
+++ asterisk_logging.py
@@ -45,7 +45,7 @@ def parse_log_line(log_line):
 
 
 def parse_log_file(log_folder_path):
-    #global latest_timestamp
+    global latest_timestamp
     latest_timestamp = None
 
     log_entries = []
