b634ceadde86770e0a81c2da10eafabfb542894c
mstark127 <97064452+mstark127@users.noreply.github.com>
2023-08-21 15:26:20 -0400

    changed sleep time

--- asterisk_logging.py
+++ asterisk_logging.py
@@ -126,6 +126,6 @@ def log_send_loop():
         #send_full_logs()  # Pass the filtered entries to send_full_logs function
         print("full logs")
         # Sleep for a while before checking logs again
-        time.sleep(2000)  # Sleep for 10 minutes
+        time.sleep(60)  # Sleep for 10 minutes
 
 log_send_loop()
