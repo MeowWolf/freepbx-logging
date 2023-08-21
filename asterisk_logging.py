7974273ee198858b3d340ac61c6578537c28aa2f
mstark127 <97064452+mstark127@users.noreply.github.com>
2023-08-21 10:52:49 -0400

    changed print(latest_timestamp) to print (entry) to see what the entry var holds as it should be one timestamp)

--- asterisk_logging.py
+++ asterisk_logging.py
@@ -126,7 +126,7 @@ def log_send_loop():
             for entry in log_entries:
                 if entry['timestamp'] > latest_timestamp:
                     filtered_entries.append(entry)
-                    print(latest_timestamp)
+                    print(entry)
         #else:
             #filtered_entries = log_entries
 
