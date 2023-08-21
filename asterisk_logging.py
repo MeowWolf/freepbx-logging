6655f151b50b2d9d0df99385aba7e40a18218652
mstark127 <97064452+mstark127@users.noreply.github.com>
2023-08-21 11:31:13 -0400

    removed comment on send_full_logs() function call for testing

--- asterisk_logging.py
+++ asterisk_logging.py
@@ -132,7 +132,7 @@ def log_send_loop():
 
         items = json.dumps(filtered_entries)
 
-        #send_full_logs()  # Pass the filtered entries to send_full_logs function
+        send_full_logs()  # Pass the filtered entries to send_full_logs function
         print("full logs")
         # Sleep for a while before checking logs again
         time.sleep(60)  # Sleep for 10 minutes
