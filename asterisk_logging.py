0344755d69d9302c3475890ad9b302d8ed16df4f
mstark127 <97064452+mstark127@users.noreply.github.com>
2023-08-21 15:27:35 -0400

    stuff

--- asterisk_logging.py
+++ asterisk_logging.py
@@ -124,7 +124,7 @@ def log_send_loop():
         items = json.dumps(filtered_entries)
 
         #send_full_logs()  # Pass the filtered entries to send_full_logs function
-        print("full logs")
+        print(items)
         # Sleep for a while before checking logs again
         time.sleep(60)  # Sleep for 10 minutes
 
