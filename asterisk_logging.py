4009ab4e9c52e0eb78fc07e7fab9bb78ee2514d6
mstark127 <97064452+mstark127@users.noreply.github.com>
2023-08-21 08:54:17 -0400

    changed PIPE to DEVNULL in stdout and stderr in the subprocess line in run_full_log_copy_as_sudo and changed the pause at the end of the log_send_loop to be 10 seconds for testing purposes

--- asterisk_logging.py
+++ asterisk_logging.py
@@ -25,7 +25,7 @@ def run_full_log_copy_as_sudo():
     sudo_command = "sudo python3 copy_logs.py"
     
     try:
-        subprocess.run(sudo_command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        subprocess.run(sudo_command, shell=True, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         print("Sudo command executed successfully.")
     except subprocess.CalledProcessError as e:
         print("Error executing sudo command:", e)
